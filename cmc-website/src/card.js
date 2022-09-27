c1f6f3278fc8a53fa276bf18d8330f2fc3f9a6b8
ClassicMC <classicmclive@gmail.com>
2022-09-27 14:47:49 +0000

    nuilhb

---
+++ cmc-website/src/card.js
@@ -0,0 +1,10 @@
+import './w3.css'
+function Card(){
+    return(
+        <div className='w3-card w3-third w3-padding w3-margin'>
+            <header>The Last Leaf</header>
+            <body>Sus</body>
+        </div>
+    );
+};
+export default Card;
